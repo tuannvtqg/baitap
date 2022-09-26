9ba67bcdc98fc2e78992d9806488abe434f7dd1e
tuanvn.tqg <tuannv.tqg@vnpt.vn>
2022-09-26 07:54:23 +0700

    bai-tap

---
+++ baitap/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿
+namespace baitap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.client_id = new System.Windows.Forms.TextBox();
+            this.client_secret = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.access_token = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.contractId = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 26);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "client_id";
+            // 
+            // client_id
+            // 
+            this.client_id.Location = new System.Drawing.Point(152, 19);
+            this.client_id.Name = "client_id";
+            this.client_id.ReadOnly = true;
+            this.client_id.Size = new System.Drawing.Size(607, 20);
+            this.client_id.TabIndex = 1;
+            this.client_id.Text = "test.client@econtract.vnpt.vn";
+            // 
+            // client_secret
+            // 
+            this.client_secret.Location = new System.Drawing.Point(152, 57);
+            this.client_secret.Name = "client_secret";
+            this.client_secret.ReadOnly = true;
+            this.client_secret.Size = new System.Drawing.Size(607, 20);
+            this.client_secret.TabIndex = 3;
+            this.client_secret.Text = "U30nrmdko76057dz5aQvV9ug0mTsqAQy";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "client_secret";
+            // 
+            // access_token
+            // 
+            this.access_token.Location = new System.Drawing.Point(152, 95);
+            this.access_token.Name = "access_token";
+            this.access_token.ReadOnly = true;
+            this.access_token.Size = new System.Drawing.Size(607, 20);
+            this.access_token.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 102);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(74, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "access_token";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(162, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 415);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(113, 23);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Create contract";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(400, 415);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(113, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Send contract";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(519, 415);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(113, 23);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Sign contract";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // contractId
+            // 
+            this.contractId.Location = new System.Drawing.Point(152, 121);
+            this.contractId.Name = "contractId";
+            this.contractId.Size = new System.Drawing.Size(607, 20);
+            this.contractId.TabIndex = 11;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 128);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "contractId";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 479);
+            this.Controls.Add(this.contractId);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.access_token);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.client_secret);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.client_id);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox client_id;
+        private System.Windows.Forms.TextBox client_secret;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox access_token;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox contractId;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ baitap/Form1.cs
@@ -0,0 +1,183 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace baitap
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string ParseAccessToken(string content) => (string)JObject.Parse(content).SelectToken("access_token");
+        private string ParseMessage(string content) => (string)JObject.Parse(content).SelectToken("message");
+        private string ParseContractId(string content) => (string)JObject.Parse(content).SelectToken("object").SelectToken("contractId");
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var client = new RestClient("https://apigateway-econtract-staging.vnptit3.vn/auth-service/oauth/token");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            var body = new
+            {
+                grant_type = "client_credentials",
+                client_id = client_id.Text,
+                client_secret = client_secret.Text
+            };
+            var jsonBody = JsonConvert.SerializeObject(body);
+
+            request.AddParameter("application/json", jsonBody, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            access_token.Text = ParseAccessToken(response.Content);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog file = new OpenFileDialog();
+            var rs = file.ShowDialog();
+            if (rs == DialogResult.OK)
+            {
+                var client = new RestClient("https://apigateway-econtract-staging.vnptit3.vn/esolution-service/contracts/create-draft-from-file-raw");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "multipart/form-data");
+                request.AddHeader("Authorization", "Bearer " + access_token.Text);
+                var customBody = new
+                {
+                    email = "test@gmail.com",
+                    sdt = "+123456789",
+                    userType = "CONSUMER",
+                    ten = "TuanTQG",
+                    noiCap = "",
+                    tenToChuc = "vnpt tuyenquang",
+                    mst = "5000254698",
+                    loaiGtId = "1",
+                    soDkdn = "",
+                    ngayCapSoDkdn = "2022-09-24",
+                    noiCapDkkd = ""
+
+                };
+                var contractBody = new
+                {
+                    autoRenew = "true",
+                    callbackUrl = "test url",
+                    contractValue = "20000",
+                    creationNote = "",
+                    endDate = "2022-09-24",
+                    sequence = 1,
+                    signFlow = new List<object>() {
+                    new
+                    {
+                        signType = "APPROVE",
+                        signForm = new List<string>(){
+                            "OTP",
+                            "EKYC",
+                            "OTP_EMAIL",
+                            "NO_AUTHEN",
+                            "EKYC_EMAIL",
+                            "USB_TOKEN",
+                            "SMART_CA"
+                        },
+                        departmentId = "",
+                        userId = "938d74f4-507d-40cf-bf0d-71d890b5a613",
+                        sequence = 1,
+                        limitDate = 3
+                    }
+                },
+                    signForm = new List<string>(){
+                            "NO_AUTHEN"
+                        },
+                    templateId = "756187ce5r5f61a09dc8ed78",
+                    title = "Hop dong tuannvtqg",
+                    validDate = "2022-09-24",
+                    verificationType = "NONE",
+                    fixedPosition = false
+                };
+
+                var jsoncustom = JsonConvert.SerializeObject(customBody);
+                var jsoncontract = JsonConvert.SerializeObject(contractBody);
+
+                request.AddParameter("customer", jsoncustom);
+                request.AddParameter("contract", jsoncontract);
+                request.AddFile("", file.FileName, "application/pdf");
+                request.AddParameter("fields", "{}");
+                IRestResponse response = client.Execute(request);
+                var mss = ParseMessage(response.Content);
+                if (mss == "ECT-00000000")
+                {
+                    contractId.Text = ParseContractId(response.Content);
+                    MessageBox.Show("Tạo thành công hợp đồng");
+                }
+
+            }
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(contractId.Text))
+            {
+                MessageBox.Show("Chưa nhập ID hợp đồng");
+                return;
+            }
+
+            var client = new RestClient($"https://apigateway-econtract-staging.vnptit3.vn/esolution-service/contracts/{contractId.Text}/submit-contract");
+            client.Timeout = -1;
+            var request = new RestRequest(Method.POST);
+            request.AddHeader("Content-Type", "application/json");
+            request.AddHeader("Authorization", "Bearer " + access_token.Text);
+            var body = @"";
+            request.AddParameter("application/json", body, ParameterType.RequestBody);
+            IRestResponse response = client.Execute(request);
+            Console.WriteLine(response.Content);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(contractId.Text))
+            {
+                MessageBox.Show("Chưa nhập ID hợp đồng");
+                return;
+            }
+            OpenFileDialog file = new OpenFileDialog();
+            var rs = file.ShowDialog();
+            if (rs == DialogResult.OK)
+            {
+                var client = new RestClient($"https://apigateway-econtract-staging.vnptit3.vn/esolution-service/contracts/{contractId.Text}/digital-sign");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Authorization", "Bearer " + access_token.Text);
+                request.AddFile("file", file.FileName, "application/pdf");
+                var body = new
+                {
+                    SignForm = "OTP_EMAIL",
+                    name = "Nguyen Viet Tuan",
+                    taxCode = "123456789",
+                    provider = "VNPT Tuyen Quang",
+                    pathImg = "iVBORw0KGgoAAAANSUhEUgAAAZAAAAJYCAYAAABM7LCIAABym",
+                    identifierCode = "123456789",
+                    phone = "123456789",
+                    email = "test@gmail.com",
+                    status = "VALID",
+                    signType = "APPROVAL",
+                    ekycInfo = "APPROVAL"
+                };
+                var jsonBody = JsonConvert.SerializeObject(body);
+                request.AddParameter("data", jsonBody);
+                IRestResponse response = client.Execute(request);
+                Console.WriteLine(response.Content);
+            }
+        }
+    }
+}
